620c5a613f9f75f03a114ca1063fa19b430e7496
Younes Saada <ys.saada12@gmail.com>
2025-03-29 02:49:38 -0400

    gettting started with react

--- react/myproject/src/App.tsx
+++ react/myproject/src/App.tsx
@@ -1,7 +1,42 @@
-export default function App() {
+import {useEffect, useState} from 'react';
+import axios, {AxiosResponse} from 'axios';
+
+const RANDOM_USER_EP = "https://randomuser.me/api/";
+
+const randomUsersApi = axios.create({
+
+  baseURL: RANDOM_USER_EP,
+  headers: {
+      "Content-Type": "application/json"
+    },
+  }
+); 
+
+
+const App = () => { 
+  const [count, setCount] = useState<number>(10);
+
+  const getRandomUsers = async () => {
+    const res = await randomUsersApi.get("/");
+    console.log("res", res);
+  };
   return (
     <div>
-      <h1>HELLO THERE</h1>
+      <span>
+        <input 
+          type="number"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
+        <button onClick={getRandomUsers}>get</button>
+      </span>
+      <h2>count: {count}</h2>
+      <h1>HOLAA</h1>
     </div>
   );
-}
+};
+
+
+
+
+export default App;
